b1b2d245dc8ae8aa4849bd05dcb2da1bab63861e
nazmul <nazmulalam9203@gmail.com>
2022-02-16 01:15:33 +0600

    go

--- js/banking.js
+++ js/banking.js
@@ -6,8 +6,8 @@
 // const first = dibblit(5);
 // const second = dibblit(7);
 
-function getinput (){
-    const inputfild = document.getElementById('deposite-input');
+function getinput(num){
+    const inputfild = document.getElementById(num);
      const inputfildtext = inputfild.value;
     const inputfildfigur = parseFloat(inputfildtext);
 
@@ -25,7 +25,7 @@ document.getElementById('diposite-button').addEventListener('click',function(){
     // const inputfild = document.getElementById('deposite-input');
     // const inputfildtext = inputfild.value;
     // const inputfildfigur = parseFloat(inputfildtext);
-    const inputfildfigur =getinput ();
+    const inputfildfigur = getinput('deposite-input');
    
 
     // get current deposite
@@ -51,9 +51,10 @@ document.getElementById('diposite-button').addEventListener('click',function(){
 // widthraw 
 
 document.getElementById('widthrow-button').addEventListener('click',function(){
-    const widthrwfild = document.getElementById('widthrow-input');
-    const widthrwfildtext = widthrwfild.value;
-    const widthrwfildfigur = parseFloat(widthrwfildtext);
+    // const widthrwfild = document.getElementById('widthrow-input');
+    // const widthrwfildtext = widthrwfild.value;
+    // const widthrwfildfigur = parseFloat(widthrwfildtext);
+    const widthrwfildfigur = getinput('widthrow-input')
 
     //withrow sowen fild
     const currentwidthraw = document.getElementById('withdraw');
